215c43ebab5f6fcc1824e8a0bb146d7543cb696c
Kristyna <hermanovak@outlook.com>
2023-04-17 14:05:30 +0200

    basic template for daily/weekly/monthly forms

--- PTC_QA/daily_QA/urls.py
+++ PTC_QA/daily_QA/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('/dailyQA', views.daily, name="daily"),
+    path('/weeklyQA', views.weekly, name="weekly"),
+    path('/monthlyQA', views.monthly, name="monthly"),
 ]
--- PTC_QA/daily_QA/views.py
+++ PTC_QA/daily_QA/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    return render(request, 'daily_QA/home.html', {})
 
+
+def daily(request):
+    return render(request, 'daily_QA/daily.html', {})
+
+def weekly(request):
+    return render(request, 'daily_QA/weekly.html', {})
+
+def monthly(request):
+    return render(request, 'daily_QA/monthly.html', {})
