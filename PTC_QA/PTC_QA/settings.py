ea9f7d91e15a7f1f1afed80296ae247a4324dfa4
Kristyna <hermanovak@outlook.com>
2023-04-17 14:41:01 +0200

    added database info into settings

--- PTC_QA/PTC_QA/settings.py
+++ PTC_QA/PTC_QA/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'PTC_QA.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#K: change the database here
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'qa_test',
+        'USER': 'root',  
+        'PASSWORD': 'password123',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
     }
 }
 
